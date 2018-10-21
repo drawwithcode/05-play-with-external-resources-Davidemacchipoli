fe56b26d40fe7ad183419c28da8d9ed7da4fd859
Davidemacchipoli <davidemacchi97@gmail.com>
2018-10-21 17:34:06 +0200

    prima prova

--- sketch.js
+++ sketch.js
@@ -1,12 +1,55 @@
+var mySong;
+var analyzer;
+var mCanvas;
+var myImg;
+
 function preload(){
-  // put preload code here
-  background(255,0,0);
+  mySong = loadSound("./assets/edCastle.mp3");
+  myImg = loadImage('./assets/flamingo.jpg');
+
 }
 
 function setup() {
-  // put setup code here
+  createCanvas(windowWidth, windowHeight);
+
+  // Two lines of magic code. The "analyzer" is a sort of "function"
+  // able to perform measurements on the song and give back values
+  analyzer = new p5.Amplitude();
+  analyzer.setInput(mySong);
 }
 
 function draw() {
-  // put drawing code here
+    mCanvas = createCanvas(windowWidth,windowHeight);
+    push();
+    image(myImg,0,0,0);
+    pop();
+  var volume = 0;
+
+    //background(0,255,0);
+
+
+    volume = analyzer.getLevel();
+    volume = map(volume,0,1,0,height/2);
+
+
+
+
+  mCanvas.mouseClicked(function(){
+    if(mySong.isPlaying()==true){
+        mySong.pause();
+    }else{
+        mySong.play();
+    }
+});
+
+
+  ellipse(width/2,height/2, volume, volume);
+
+
+
+}
+
+
+function windowResized(){
+  resizeCanvas(windowWidth,windowWidth);
 }
